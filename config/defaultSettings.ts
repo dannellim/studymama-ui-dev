05d1a522747a5fece84d6fe69ae9a8034697c06e
prakashkaul <prakash.kaul.16@gmail.com>
2021-04-23 13:29:32 +0800

    initial version

---
+++ config/defaultSettings.ts
@@ -0,0 +1,22 @@
+import { Settings as ProSettings } from '@ant-design/pro-layout';
+
+type DefaultSettings = Partial<ProSettings> & {
+  pwa: boolean;
+};
+
+const proSettings: DefaultSettings = {
+  navTheme: 'dark',
+  primaryColor: '#1890ff',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'StudyMama',
+  pwa: false,
+  iconfontUrl: '',
+};
+
+export type { DefaultSettings };
+
+export default proSettings;
